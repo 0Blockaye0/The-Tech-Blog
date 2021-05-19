bf5d4129a2e7add0c2e81415e1b663ad477dcdd4
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-19 11:43:07 -0500

    debug

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
+// const addpostRoutes = require('./addpost-routes');
 
+// router.use('/addpost', addpostRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
