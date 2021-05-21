b85abf06590400138cabe5909c003683f1069872
Blake Johnson <blakejohnson@Blakes-MBP.attlocal.net>
2021-05-21 14:49:36 -0500

    added signup btn to login page. created signup view and js

---
+++ public/javascript/signup.js
@@ -0,0 +1,38 @@
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+async function signupRedirect(event) {
+  event.preventDefault();
+  document.location.replace("/signup/");
+}
+
+document
+  .querySelector("#signupLinkBtn")
+  .addEventListener("click", signupRedirect);
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
